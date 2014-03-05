e11ce85068a827b559fca588fc0f421f55d3efe2
James Lal <james@lightsofapollo.com>
2014-03-04 23:07:34 -0800

    moar comments

--- schema.js
+++ schema.js
@@ -1,13 +1,16 @@
 var Schema = require('amqpworkers/schema');
 var debug = require('debug')('build queue');
 
+// WHY 8 hours??? That is roughly twice the time between mozilla central builds.
+// If we are down for more then 8 hours we don't care about those results
+// anyway.
 var MESSAGE_TTL_MS =
    // one minute
    (1000 * 60) *
    // one hour
    60 *
    // four hours
-   4;
+   8;
 
 function buildQueue(queue, durable) {
   debug('building schema for: ', queue);
@@ -17,8 +20,10 @@ function buildQueue(queue, durable) {
   };
 
   if (durable) {
+    // durable === never delete this queue
     queueConfig.durable = true;
   } else {
+    // not durable means delete this crap as soon as nobody is listening
     queueConfig.autoDelete = true;
     queueConfig.durable = false;
   }
